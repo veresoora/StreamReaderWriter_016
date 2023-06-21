3f70d7639786ac046d069bb4c38bef6a77553886
veresoora <lahanabira@gmail.com>
2023-06-21 15:49:19 +0700

    memasukkan obyek exception dan array

---
+++ exceptionStandart/exceptionStandart.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <exception>
+//untuk obyek exception yang akan digunakan
+#include <array>
+//untuk obyek array yang akan kita gunakan
+using namespace std;
+int main()
+{
+
+}
