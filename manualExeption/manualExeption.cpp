50e172ff684ec4bdaa4a68a4fa1434f4e2918068
veresoora <lahanabira@gmail.com>
2023-06-21 15:42:58 +0700

    blok akan dieksekusi

--- manualExeption/manualExeption.cpp
+++ manualExeption/manualExeption.cpp
@@ -8,6 +8,9 @@ int main()
 		throw 0.5; //melemparkan sebuah integer maka
 		cout << "Pernyataan tidak akan dieksekusi" << endl;
 	}
-	
+	catch (int a) {
+		//blok ini akan dieksekusi
+		cout << "Pengecualian akan dieksekusi" << endl;
+	}
 
 }
