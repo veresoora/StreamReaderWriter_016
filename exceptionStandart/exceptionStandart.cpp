14e165f5ef87ece29d81681fac8ee466a10c602a
veresoora <lahanabira@gmail.com>
2023-06-21 15:52:43 +0700

    menambahkan penanda 1

--- exceptionStandart/exceptionStandart.cpp
+++ exceptionStandart/exceptionStandart.cpp
@@ -6,5 +6,11 @@
 using namespace std;
 int main()
 {
-
+	cout << "Awal Program" << endl; //penanda 1:....
+	try {
+		array<int, 3> data = { 3, 5, 7 };
+		//pesan array integer 3 elemen
+		cout << data.at(5) << endl;
+		//memanggil array elemen ke 5
+	}
 }
