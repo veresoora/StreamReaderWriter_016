694c39a3f0c7f1d8c4081dfa5f7297aa8a794a81
veresoora <lahanabira@gmail.com>
2023-06-21 15:42:41 +0700

    melemparkan sebuah integer maka pernyataan tidak akan dieksekusi

---
+++ manualExeption/manualExeption.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	try {
+		cout << "Selamat Belajar di Prodi TI UMY" << endl;
+		throw 0.5; //melemparkan sebuah integer maka
+		cout << "Pernyataan tidak akan dieksekusi" << endl;
+	}
+	
+
+}
